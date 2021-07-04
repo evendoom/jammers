47c7565b266adc55236cb108c8cf35540f576e57
evendoom <davidej.p.correia@gmail.com>
2021-07-04 17:18:58 +0000

    feat: Add active link status to dashboard nav links

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,11 @@
-// Dropdown menu for mobile
 $(document).ready(function() {
-    $(".dropdown-trigger").dropdown();   
-});
-
-// Modal for feedback
-$(document).ready(function(){
+    // Dropdown menu for mobile
+    $(".dropdown-trigger").dropdown();
+    
+    // Modal for messages and feedback
     $('.modal').modal();
-});
 
-// Resize textbox for sending message modal
-$(document).ready(function() {
+    // Resize textbox for sending message modal
     $('.autoResize').on('input', function() {
         this.style.height = '100px';
         this.style.height = (this.scrollHeight) + 'px';
