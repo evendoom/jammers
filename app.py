85868aab7b30c94bf0dbc9b796e943e01c776f6d
evendoom <davidej.p.correia@gmail.com>
2021-07-11 13:26:23 +0000

    feat: Add about page

--- app.py
+++ app.py
@@ -610,6 +610,12 @@ def check_new_messages():
     return new_messages
 
 
+# About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')))
